03172ada74990bce0c1f5a16d5d2d51613e6f58b
NwekeFavour <buchijunior90@gmail.com>
2026-05-21 11:53:41 +0100

    progress

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,24 @@ const { supabaseAdmin } = require('./config/db');
 const jwt = require('jsonwebtoken');
 
 app.use(helmet());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
-  credentials: true,
-}));
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_URL_STAGING,
+  process.env.FRONTEND_URL_PROD,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }) 
+);
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
